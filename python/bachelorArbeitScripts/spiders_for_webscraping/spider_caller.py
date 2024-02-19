f94828fb4769fa8833510703df4b4d707346b72a
windhund-hub <57817667+windhund-hub@users.noreply.github.com>
2024-02-19 22:18:58 +0100

    Delete python/bachelorArbeitScripts directory

--- python/bachelorArbeitScripts/spiders_for_webscraping/spider_caller.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-spiders = ['spider']
-for spider in spiders:
-    myprocess = subprocess.Popen(["scrapy","crawl", spider], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = myprocess.communicate()
-    status = myprocess.wait()
