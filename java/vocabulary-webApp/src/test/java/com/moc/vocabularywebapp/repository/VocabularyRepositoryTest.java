5cff923b984bf6c21fc30fdddb198c567f8f2efa
windhund-hub <57817667+windhund-hub@users.noreply.github.com>
2024-02-22 22:58:40 +0100

    adding repository tests

---
+++ java/vocabulary-webApp/src/test/java/com/moc/vocabularywebapp/repository/VocabularyRepositoryTest.java
@@ -0,0 +1,71 @@
+package com.moc.vocabularywebapp.repository;
+
+import com.moc.vocabularywebapp.model.Vocabulary;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
+
+@DataJpaTest
+class VocabularyRepositoryTest {
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Autowired
+    private VocabularyRepository vocabularyRepository;
+
+    @BeforeEach
+    void setUp() {
+        //Arrange
+        Vocabulary vocabulary = new Vocabulary("Hund", "dog");
+        Vocabulary vocabulary2 = new Vocabulary("Elefant", "elephant");
+        Vocabulary vocabulary3 = new Vocabulary("Adler", "eagle");
+        vocabularyRepository.save(vocabulary);
+        vocabularyRepository.save(vocabulary2);
+        vocabularyRepository.save(vocabulary3);
+    }
+
+    @Test
+    void testFindExpression_whenGivenSubstring_returnsCorrectExpressions() {
+
+
+        // Act
+        List<Vocabulary> storedVocabulary = vocabularyRepository.findExpression("n");
+
+        // Assert
+        assertThat(storedVocabulary).hasSize(2).extracting(Vocabulary::getExpression)
+                .containsOnly("Hund", "Elefant");
+    }
+
+    @Test
+    void testFindTranslation_whenGivenSubstring_returnsCorrectTranslation() {
+
+        // Act
+        List<Vocabulary> storedVocabulary = vocabularyRepository.findTranslation("n");
+
+        // Assert
+        assertThat(storedVocabulary).hasSize(1).extracting(Vocabulary::getTranslation)
+                .containsOnly("elephant");
+
+    }
+
+    @Test
+    void testFindIdByExpression_whenGivenExpression_returnsCorrectId() {
+        Vocabulary v = new Vocabulary("Hase", "rabbit");
+        //vocabularyRepository.save(v);
+        testEntityManager.persistAndFlush(v);
+        Vocabulary storedVocabulary = vocabularyRepository.findIdByExpression(v.getExpression());
+
+        // Assert
+        Assertions.assertEquals(10, storedVocabulary.getId(),
+                "Id should be 10");
+
+
+
+    }
+}
